505189d904f78d0794a55258e0c1e159f8c1faa0
Mama-Thomas <tmama20012001@gmail.com>
2022-04-27 12:14:58 -0700

    set up the routes, and pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+import Navbar from "./components/navbar/navbar";
+import Footer from "./components/footer/footer";
+import Homepage from "./pages/Homepage";
+import Catalog from "./pages/Catalog";
+import Detail from "./pages/Detail/Detail";
+
 
 function App() {
   return (
-      <div>App</div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/:category" element={<Catalog />} />
+          <Route path="/:category/search/:keyword" element={<Catalog />} />
+          <Route path="/:category/:id" element={<Detail />} />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
