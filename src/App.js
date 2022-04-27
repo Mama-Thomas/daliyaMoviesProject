ffbfadf8786b0b21ed17ea59f6e51638c8d26300
Mama-Thomas <tmama20012001@gmail.com>
2022-04-27 15:29:27 -0700

    added and styled the button component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
+import "./App.scss";
 
 import Navbar from "./components/navbar/navbar";
 import Footer from "./components/footer/footer";
