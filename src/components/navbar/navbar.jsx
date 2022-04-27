60219a16a2ad9175b9cca777408802d345c38869
Mama-Thomas <tmama20012001@gmail.com>
2022-04-27 13:45:21 -0700

    added and styled the navigation bar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import './navbar.scss';
+import logo from '../../assets/playIcon.png'
+import { useNavigate } from 'react-router-dom';
+
+
 
 const Navbar = () => {
+
+    const navigate = useNavigate();
+
   return (
-    <div>Navbar</div>
-  )
+    <div onClick={() => navigate("/")} className="navbar wrap">
+      <div className="logo">
+        <img src={logo} alt="" />
+        DaliyaMovies
+      </div>
+      <div>
+        <ul className="navlist">
+          <li onClick={() => navigate("/movie")}>Movies</li>
+          <li onClick={() => navigate("/tv")}>Tv Shows</li>
+          <li onClick={() => navigate('/people')}>People</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
