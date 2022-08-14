8753f110573c860f6f588dfd2a76280c78b5b5d5
Mama-Thomas <tmama20012001@gmail.com>
2022-08-14 13:16:53 -0700

    Updated and  Styled tg=he footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,29 +1,54 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
 import logo from '../../assets/playIcon.png';
 
-import './footer.scss'
+import './footer.scss';
+
+import { Link } from 'react-router-dom';
+
+
 
 
 const Footer = () => {
 
-  const navigate = useNavigate();
-
   return (
     <div className="footer">
-      <div className="content">
-        <div className="logo2">
-          <img src={logo} alt="" />
-          <p>DaliyaMovies</p>
+      <div className="footerContentContainer">
+        <div className="footerContentLogo">
+          <div className="logo2">
+            <img src={logo} alt="" />
+            <p>DaliyaMovies</p>
+          </div>
         </div>
-        <div>
+
+        <div className="footerListItems">
+          <div className="footerListItem">
+            <Link to="/">Home</Link>
+            <Link to="/">Contact Us</Link>
+            <Link to="/">Terms of Services</Link>
+            <Link to="/">About us</Link>
+          </div>
+
+          <div className="footerListItem">
+            <Link to="/">FAQ</Link>
+            <Link to="/">Privacy Policy</Link>
+            <Link to="/">Premium</Link>
+           </div>
+          <div className="footerListItem">
+            <Link to="/">Must Watch</Link>
+            <Link to="/">Latest</Link>
+            <Link to="/">Popular</Link>
+          </div>
+           
+        </div>
+
+        {/* <div>
           <ul className="footerList">
             <li onClick={() => navigate("/")}>Home</li>
             <li onClick={() => navigate("/")}>Terms of Service</li>
             <li onClick={() => navigate("/")}>Privacy Policy</li>
             <li onClick={() => navigate("/")}>FAQ</li>
           </ul>
-        </div>
+        </div> */}
       </div>
     </div>
   );
